9548093692fb4866751800d4168456127020805e
Victor Krasnohorskiy <hedgefix@gmail.com>
2016-07-06 13:20:15 +0300

    Initial Commit

---
+++ CoreDataTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreDataTest
+//
+//  Created by Виктор on 06.07.16.
+//  Copyright © 2016 Hegefix. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
